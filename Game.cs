0c256b297b5e9c328c4db340edc516872b8c98e2
Michael Closky <mikeclosky29@gmail.com>
2022-07-20 22:00:42 -0400

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissor
+{
+    internal class Game
+    {
+        public static List<string> gestures = new List<string>() {"", "Rock", "Paper", "Scissors"};
+        public string userChoice;
+        public string computerChoice;
+
+        public Game(int userChoice)
+        {
+            this.userChoice = gestures[userChoice];
+
+            Random random  = new Random();
+            int randomNumber = random.Next(1,4);
+            computerChoice = gestures[randomNumber];
+        }
+
+        public string winner()
+        {
+            if(userChoice == computerChoice)
+            {
+                Console.WriteLine($"both players chose {userChoice}");
+                return "draw";
+            } else if(userChoice == "Rock" && computerChoice == "Paper" || userChoice == "Scissors" && computerChoice == "Rock" || userChoice == "Paper" && computerChoice == "Scissors")
+            {
+                Console.WriteLine($"computer won with {computerChoice} and user picked {userChoice}");
+                return "computer";
+            } else if(userChoice == "Paper" && computerChoice == "Rock" || userChoice == "Rock" && computerChoice == "Scissors" || userChoice == "Scissors" && computerChoice == "Paper")
+            {
+                Console.WriteLine($"user won with {userChoice} and computer picked {computerChoice}");
+                return "user";
+            }
+            return "this should never return";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace RockPaperScissor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userWins = 0;
+            int compWins = 0;
+
+            while(userWins < 3 && compWins < 3)
+            {
+                Console.WriteLine($"PLayer Wins: {userWins} | Computer Wins: {compWins}");
+                Console.WriteLine("What would you like to throw?");
+                Console.WriteLine("1) Rock");
+                Console.WriteLine("2) Paper");
+                Console.WriteLine("3) Scissors");
+                string gesture = Console.ReadLine();
+                int numberForConstructor = int.Parse(gesture);
+
+                Game game = new Game(numberForConstructor);
+               string winner =  game.winner();
+               if(winner == "computer")
+                {
+                    compWins++;
+                } else if(winner == "user")
+                {
+                    userWins++;
+                }
+            }
+
+        }
+    }
+}
